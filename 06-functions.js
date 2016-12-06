942ba11d15e36324d4b32fc64ed71498e62768c1
Eric Weitz <eric.weitz@nih.gov>
2016-12-06 17:26:39 -0500

    Remove extraneous whitespace

--- 06-functions.js
+++ 06-functions.js
@@ -1,7 +1,7 @@
 // This function takes a parameter "gene".
 // "gene" is an object, and we use its properties to
 // return a string representation of the gene's location
-function stringifyLocation(gene) {
+function stringifyLocation(gene) {
   return gene.chromosome + ':' + gene.start + '-' + gene.stop;
 }
 
